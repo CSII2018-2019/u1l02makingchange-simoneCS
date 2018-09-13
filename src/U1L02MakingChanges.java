113e4446264d344d87f440f927ec7dfda4c4866d
rothsteins <rothsteins@LR064W78-1718.ellis.int>
2018-09-13 09:31:48 -0400

    Mathematical Method the pennies might not be working

--- src/U1L02MakingChanges.java
+++ src/U1L02MakingChanges.java
@@ -3,6 +3,8 @@ import javax.swing.JOptionPane;
 public class U1L02MakingChanges {
 
 	public static void main(String[] args) {
+		/* USING A LOOP
+		 * 
 		//define variables for amounts of each coin
 		int nickels = 0; //initializing nickels 
 		int pennies = 0; 
@@ -26,6 +28,27 @@ public class U1L02MakingChanges {
 		JOptionPane.showMessageDialog(null, " $ " + cash + " is " + 
 		nickels + " nickels " 
 		+ pennies + " pennies ");
+		*/
+		
+		
+		//MATHEMATICAL METHOD 
+		int nickels = 0; //initializing nickels 
+		int pennies = 0; 
+		//get input form user 
+		String answer = JOptionPane.showInputDialog(" Enter a monetary value (ex: 1.56): ");
+		//System.out.println(answer); // just to check if did right 
+		double cash = Double.parseDouble(answer);
+		
+		//divide by 5 and round down 
+		nickels = (int)(cash/0.05); //need to use an int so it's chopping off decimals
+		System.out.println(nickels);
+		
+		//subtract # nickels * 5 from total to get pennies 
+		pennies = (int)(cash - .05 * nickels); 
+		//print 
+		JOptionPane.showMessageDialog(null, " $ " + cash + " is " + 
+				nickels + " nickels and " 
+				+ pennies + " pennies ");
 	}
 
 }
