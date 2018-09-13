a4b9de5963b01e9f2287433be0ca852913ee2dc4
rothsteins <rothsteins@LR064W78-1718.ellis.int>
2018-09-13 09:18:35 -0400

    saved

--- src/U1L02MakingChanges.java
+++ src/U1L02MakingChanges.java
@@ -1,9 +1,31 @@
+import javax.swing.JOptionPane;
 
 public class U1L02MakingChanges {
 
 	public static void main(String[] args) {
-		//pseudocode 
-
+		//define variables for amounts of each coin
+		int nickels = 0; //initializing nickels 
+		int pennies = 0; 
+		
+		//get input from user (change to usable volume)
+		String answer = JOptionPane.showInputDialog(" Enter a monetary value (ex: 1.56): ");
+		//System.out.println(answer); // just to check if did right 
+		double cash = Double.parseDouble(answer);
+		pennies = (int)(cash * 100); // don't need itn again cause already defined 
+		System.out.println(pennies);
+		
+		//loop subtracting coin amount until $ < coin amount
+		while (pennies > 5) {
+			nickels++;
+			pennies -=5; 
+		}
+		
+		//pennies = money *100 <-- don't need b/c converted to integer value 
+		
+		//print answer 
+		JOptionPane.showMessageDialog(null, " $ " + cash + " is " + 
+		nickels + " nickels " 
+		+ pennies + " pennies ");
 	}
 
 }
