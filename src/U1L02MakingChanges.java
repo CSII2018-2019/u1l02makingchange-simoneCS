07e7f70c565afa22193df2f790267056fdeb87d8
rothsteins <rothsteins@LR064W78-1718.ellis.int>
2018-09-23 17:52:41 -0400

    This works most of the time

--- src/U1L02MakingChanges.java
+++ src/U1L02MakingChanges.java
@@ -34,31 +34,74 @@ public class U1L02MakingChanges {
 		//MATHEMATICAL METHOD 
 		//quarters nickels dimes and pennies 
 		int nickels = 0; //initializing nickels 
-		int pennies = 0; 
-		int quarters = 0;
+		int pennies = 0;
 		int dimes = 0;
+		int quarters1 = 0;
+		int quarters2 = 0;
+		int average = 0;
+		int answer1 = 0;
 		//get input form user 
-		String answer = JOptionPane.showInputDialog(" Enter a monetary value (ex: 1.56): ");
+		String answer = JOptionPane.showInputDialog(" Enter amount of dollars (ex: 1.00): ");
+		String answer2 = JOptionPane.showInputDialog(" Enter amount of cents (ex: 0.56) " );
 		//System.out.println(answer); // just to check if did right 
-		double cash = Double.parseDouble(answer);
+		double dollar = Double.parseDouble(answer);
+		double cent = Double.parseDouble(answer2);
 		
+		/*
 		//divide by .25 and round down
 		quarters = (int)(cash/.25);
 		
-		//subtract # quarters from cash and then divide by.1
-		//dimes = (int)(cash-quarters *.1);
-		
 		//divide by .05 and round down 
-			nickels = (int)(cash - quarters *.25); //need to use an int so it's chopping off decimals
-		//System.out.println(nickels);
+		cash = (double)(cash - quarters * .25); //need to use an int so it's chopping off decimals
+		System.out.println(pennies);
+		
+		nickels = (int)(pennies/.05);
+		System.out.println(nickels);
 		
 		//subtract # nickels * 5 from total to get pennies 
-		pennies = (int)(cash - nickels * .05); 
+		cash = (double)(cash - pennies * .05); 
+		System.out.println(pennies);
+		
+		pennies = (int)(cash * 100);
 		
 		//print 
-		JOptionPane.showMessageDialog(null, " $ " + cash + " is " + 
+		JOptionPane.showMessageDialog(null, " $ " + 
 				quarters + " quarters, " +
 				nickels + " nickels, and " 
 				+ pennies + " pennies ");
+		*/
+		//MODULAR METHOD 
+		average = (int)(dollar + cent/2);
+		
+		//if (dollar%.25 == 0); {
+			quarters1 = (int)(dollar/.25); 
+		//}
+		
+		answer1 = (int)(cent%.25);
+		//if (answer1 == 0); {
+			quarters2 = (int)(cent/.25);
+			nickels = 0;
+			dimes = 0;
+			pennies = 0;//}
+		
+		answer1 = (int)(answer1%.1);
+		//if (answer1 ==0); {
+			nickels = 0;
+			dimes = (int)(answer1/.1);
+			pennies = 0;//}
+		
+		answer1 = (int)(answer1%.05);
+		//if (answer1 ==0 ); {
+			nickels = (int)(answer1/.05);
+			pennies = 0;//}
+		
+		answer1 = (int)(answer1%.01);
+		//if (answer1 ==0); {
+			pennies = (int)(answer1/.01);
+		//}
+		
+		JOptionPane.showMessageDialog(null, " quarters = " + (quarters1 + quarters2)
+				+ " dimes = " + dimes + "nickles = " + nickels + " pennies = " + pennies
+				+ " The average is " + average);
 	}
 }
